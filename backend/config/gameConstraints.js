449d1972c3b8cde7dbb14a9993577022c2318adf
michelaPaiarin <michela.paiarin@gmail.com>
2026-04-17 17:06:15 +0200

    Extrapolate game configuration rules for greater cleanliness

---
+++ backend/config/gameConstraints.js
@@ -0,0 +1,9 @@
+export const GAME_CONSTRAINTS = {
+    RequireFields: ["name", "minPlayer", "maxPlayer", "time", "location"],
+    OptionalFields: ["description", "bigImage", "smallImage", "year"],
+    MinPlayers: 1,
+    MinTime: 1,
+    MinYear: 1900,
+    MaxYear: new Date().getFullYear(),
+    LocationRegex: /^[A-Z]\.\d+\.\d+$/,
+    ImageAcceptedProtocols: ['http:', 'https:']
