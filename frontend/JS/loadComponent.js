0569dcb2be4b29e7ac0ca19282a7a782d5b6adf7
michelaPaiarin <michela.paiarin@gmail.com>
2026-04-27 12:07:17 +0200

    Refactor component loading to handle multiple components dynamically

--- frontend/JS/loadComponent.js
+++ frontend/JS/loadComponent.js
@@ -1,15 +1,25 @@
 const NAVBAR_PATH = './components/navbar.html';
+const FOOTER_PATH = './components/footer.html';
 
-async function loadNavbar() {
+const load = [{'path': NAVBAR_PATH, 'placeholderId': 'navbar-placeholder'},
+              {'path': FOOTER_PATH, 'placeholderId': 'footer-placeholder'}];
+
+async function loadComponents() {
+    for (const item of load) {
+        loadComponent(item.path, item.placeholderId);
+    }
+}
+
+async function loadComponent(path, placeholderId) {
     try {
-        const response = await fetch(NAVBAR_PATH);
-        if (!response.ok) throw new Error('Errore nel caricamento della navbar');
+        const response = await fetch(path);
+        if (!response.ok) throw new Error(`Errore nel caricamento di ${path}`);
         const html = await response.text();
-        document.getElementById('navbar-placeholder').innerHTML = html;
+        document.getElementById(placeholderId).innerHTML = html;
     } catch (error) {
-        console.error("Non sono riuscito a caricare la navbar:", error);
+        console.error(`Non sono riuscito a caricare ${path}:`, error);
     }
 }
 
 // Chiama la funzione quando la pagina si carica
-document.addEventListener('DOMContentLoaded', loadNavbar);
+document.addEventListener('DOMContentLoaded', loadComponents);
