13ba7c935b6c3fdcf70390827d7ab81c9caf35e8
michelaPaiarin <michela.paiarin@gmail.com>
2026-05-14 21:01:55 +0200

    Enhance game form validation and error handling with improved input fields and error messages

--- frontend/JS/views/gameForm.js
+++ frontend/JS/views/gameForm.js
@@ -1,6 +1,8 @@
-import { loadAllGameList } from '../main.js';
-import { getGameById } from '../utilities/api.js';
-import { validateGame, cleanGameData, GAME_CONSTRAINTS } from '../sharedExports.js';
+import { loadAllGameList                                } from '../main.js';
+import { getGameById, postGame, putGame                 } from '../utilities/api.js';
+import { validateGame, cleanGameData, GAME_CONSTRAINTS  } from '../sharedExports.js';
+
+const errorSufix = '-error';
 
 const ID = {
     Name: 'name-input',
@@ -26,12 +28,11 @@ const IDMaximalConstraints = {
 
 // note: if error occurs the caller handle it
 export async function fillGameForm(id) {
-    const game = await getGameById(id);
-    await fillGameFormWithGame(game);
+    await fillGameFormWithGame(await getGameById(id));
 }
 
 async function fillGameFormWithGame(game) {
-    console.log("Riempio il form con i dati del gioco:", game);
+    console.log("Riempio il form con i dati del gioco");
     for (const key in ID) {
         const element = document.getElementById(ID[key]);
         
@@ -61,13 +62,92 @@ export async function setConstraintGameForm() {
     addSpecificProperties('max', IDMaximalConstraints);
 }
 
-export async function gameSaveForm(event) {
-    event.preventDefault(); 
+function clearErrorMessages() {
+    for (const key in ID) {
+        let attribute;
 
-    const form = event.target; 
+        if      (key == 'MinPlayer')  { attribute = 'Players'; }
+        else if (key == 'MaxPlayer')  { continue;             }
+        else                          { attribute = key;      }
+        
+        const errorElement = document.getElementById(attribute + errorSufix);
+        if (errorElement) { errorElement.textContent = ''; }
+    }
+
+    for(const key in ID){ document.getElementById(ID[key]).classList.remove('invalid'); }
+}
+
+function showErrorMessage(field, message) {
+    const errorElement = document.getElementById(field + errorSufix);
+    if (errorElement) { errorElement.textContent = message; }
+
+    if (field == 'Player') {
+        document.getElementById(ID['MinPlayer']).classList.add('invalid');
+        document.getElementById(ID['MaxPlayer']).classList.add('invalid');
+    } else {
+        console.log(field, ID[field]);
+        document.getElementById(ID[field]).classList.add('invalid');
+    }
+}
+
+function showValidationErrors(errors) {
+    for (const error of errors) {
+        console.log(`Errore di validazione per ${error.field}: ${error.message}`);
+        showErrorMessage(error.field, error.message);
+    }
+}
+
+export async function gameSaveForm(event, previusPage) {
+    event.preventDefault(); 
+    clearErrorMessages();
+
     const method = event.target.dataset.method;
-    const gameData = Object.fromEntries(new FormData(form));
+    const gameID = event.target.dataset.gameID || null;
+    let gameData = Object.fromEntries(new FormData(event.target));
 
     console.log("Operazione:", method);
     console.log("Dati estratti dal form:", gameData);
+    
+    // To be safe. The user shouldn't see it unless they hit F12 and change the HTML5 constraints.
+    try {
+        gameData = cleanGameData(gameData);
+    } catch (error) {
+        console.error("Errore durante la pulizia dei dati del gioco:", error);
+        alert("Errore nei dati inseriti: " + error.message);
+        return;
+    }
+
+    
+    console.log("Dati puliti del form:", gameData);
+
+    let validation;
+
+    switch (method) {
+        case 'POST' : validation = validateGame(gameData);              break;
+        case 'PUT'  : validation = validateGame(gameData, true);        break;
+        default     : console.error("Metodo non supportato:", method);  throw new Error("Metodo non supportato");
+    }
+    
+    console.log("Risultato della validazione:", validation);
+
+    if (validation.errors.length > 0) { showValidationErrors(validation.errors); return; }
+    
+    saveChange(gameData, method, previusPage, gameID);
+}
+
+async function saveChange(game, method, previusPage, id = null) {
+    console.log('salvo gioco id: ', id);
+    try {
+        switch (method) {
+            case 'POST' : await postGame(game); break;
+            case 'PUT'  : await putGame(id, game); break;
+            default     : throw new Error("Metodo non supportato");
+        }
+
+        alert("Operazione completata con successo! Puoi tornare alla pagina precedente.");
+        previusPage();
+    } catch (error) {
+        console.error("Errore durante il salvataggio del gioco:", error);
+        alert("Errore durante il salvataggio del gioco: " + error.message);
+    }
 }
