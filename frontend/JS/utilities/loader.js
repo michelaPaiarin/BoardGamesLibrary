800ee890827a8fa4a0c317602c44de5ef07aaae9
kelami <michela.paiarin@gmail.com>
2026-05-19 13:13:08 +0200

    Refactor loader.js to use objects for paths and titles for improved maintainability

--- frontend/JS/utilities/loader.js
+++ frontend/JS/utilities/loader.js
@@ -1,21 +1,19 @@
-const NAVBAR_PATH = './components/navbar.html';
-const FOOTER_PATH = './components/footer.html';
-const POPUP_PATH = './components/popup.html';
+const PATHS = {
+    navbar:  './components/navbar.html',        footer:  './components/footer.html',
+    popup:   './components/pop-up.html',        formGame:    './views/mainFormGame.html',
+    allGames:    './views/mainAllGames.html',   detailGame:  './views/mainDetailGame.html',
+}
 
-const MAIN_ALL_GAMES_PATH = './views/mainAllGames.html';
-const MAIN_FORM_GAMES_PATH = './views/mainFormGame.html';
-const MAIN_DETAIL_GAME_PATH = './views/mainDetailGame.html';
-
-const MAIN_ALL_GAMES_TITLE = 'Tutti i giochi';
-const MAIN_ADD_GAME_TITLE = `Aggiungi nuovo gioco`;
-const MAIN_MODIFIED_GAMES_TITLE = `Modifica gioco: `;
-const MAIN_DETAIL_GAME_TITLE = `Dettagli gioco: `;
+const TITLES = {
+    allGames:   'Tutti i giochi',               addGame:    'Aggiungi nuovo gioco',
+    editGame:   'Modifica gioco: ',             detailGame: 'Dettagli gioco: ',
+}
 
 const MAIN_TITLE_ID = 'content-title';
 
-const loadOnce = [{'path': NAVBAR_PATH, 'placeholderId': 'navbar-placeholder'},
-                  {'path': FOOTER_PATH, 'placeholderId': 'footer-placeholder'},
-                  {'path': POPUP_PATH, 'placeholderId': 'popup_placeholder'}];
+const loadOnce = [{'path': PATHS.navbar, 'placeholderId': 'navbar-placeholder'},
+                  {'path': PATHS.footer, 'placeholderId': 'footer-placeholder'},
+                  {'path': PATHS.popup, 'placeholderId': 'popup-placeholder'}];
 
 export async function init() { for (const item of loadOnce) { await loadComponent(item.path, item.placeholderId); }}
 
@@ -33,26 +31,26 @@ export async function loadComponent(path, placeholderId) {
 export async function loadMainAllGames(){
     const titleEl = document.getElementById(MAIN_TITLE_ID);
     
-    if(titleEl) { titleEl.innerText = MAIN_ALL_GAMES_TITLE; }
-    await loadComponent(MAIN_ALL_GAMES_PATH, 'main-placeholder');
+    if(titleEl) { titleEl.innerText = TITLES.allGames; }
+    await loadComponent(PATHS.allGames, 'main-placeholder');
 }
 
 export async function loadMainAddGame(gameName){
     const titleEl = document.getElementById(MAIN_TITLE_ID);
     
-    if(titleEl) { titleEl.innerText = `${MAIN_ADD_GAME_TITLE} ${gameName}`; }
+    if(titleEl) { titleEl.innerText = `${TITLES.addGame} ${gameName}`; }
     else        { console.warn(`Non trovo l'ID ${MAIN_TITLE_ID} nell'HTML, ma carico la pagina lo stesso!`); }
 
-    await loadComponent(MAIN_FORM_GAMES_PATH, 'main-placeholder');
+    await loadComponent(PATHS.formGame, 'main-placeholder');
 }
 
 export async function loadMainModifiedGames(gameName){
     const titleEl = document.getElementById(MAIN_TITLE_ID);
     
-    if(titleEl) { titleEl.innerText = `${MAIN_MODIFIED_GAMES_TITLE} ${gameName}`; }
+    if(titleEl) { titleEl.innerText = `${TITLES.editGame} ${gameName}`; }
     else        { console.warn(`Non trovo l'ID ${MAIN_TITLE_ID} nell'HTML, ma carico la pagina lo stesso!`); }
     
-    await loadComponent(MAIN_FORM_GAMES_PATH, 'main-placeholder');
+    await loadComponent(PATHS.formGame, 'main-placeholder');
 }
 
 export async function loadMainDetailGame(gameName){
@@ -61,5 +59,5 @@ export async function loadMainDetailGame(gameName){
     if(titleEl) { titleEl.innerText = `${MAIN_DETAIL_GAME_TITLE} ${gameName}`; }
     else        { console.warn(`Non trovo l'ID ${MAIN_TITLE_ID} nell'HTML, ma carico la pagina lo stesso!`); }
     
-    await loadComponent(MAIN_DETAIL_GAME_PATH, 'main-placeholder');
+    await loadComponent(PATHS.detailGame, 'main-placeholder');
 }
