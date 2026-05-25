a8fc17316d4c749177cbca65755d92833cf56caa
michelaPaiarin <michela.paiarin@gmail.com>
2026-05-25 17:37:52 +0200

    Refactoring the searchBar with cleaner code

--- frontend/JS/components/searchBar.js
+++ frontend/JS/components/searchBar.js
@@ -1,29 +1,40 @@
 import { updateFiltersAndRefresh, FILTER_ACTION } from "../utilities/filterManager.js";
+import { showUpcomingFeatures } from "../utilities/notifier.js"
 
 const SEARCH_INPUT_ID = 'search-game-input';
-const SEARCH_BTN_ID   = 'search-btn';
-const CLEAR_BTN_ID    = 'clear-filter-btn';
+const BTN = {
+    search:   { id: 'search-btn',          onclick: searchGame            },
+    clear:    { id: 'clear-filter-btn',    onclick: clearSearchName       },
+    advanced: { id: 'advanced-search-btn', onclick: showUpcomingFeatures  } 
+};
 
 export function initSearchBar() {
-    const input   = document.getElementById(SEARCH_INPUT_ID);
-    const searchBtn = document.getElementById(SEARCH_BTN_ID);
-    const clearBtn  = document.getElementById(CLEAR_BTN_ID);
-
-    searchBtn.addEventListener('click', () => {
-        const value = input.value.trim();
-        if (!value) {  input.setCustomValidity('Inserisci un titolo da cercare'); input.reportValidity();  return; }
-        input.setCustomValidity(''); // reset errore
-        updateFiltersAndRefresh({ Name_c: value }, FILTER_ACTION.ADD);
-
-    });
-
-    clearBtn.addEventListener('click', () => {
-        input.value = '';
-        updateFiltersAndRefresh({ Name_c: null }, FILTER_ACTION.REMOVE);
+    Object.values(BTN).forEach(config => {
+        const button = document.getElementById(config.id);
+        if (!button) {console.warn(`${config.id} not found`); } // Checking to be safe. It shouldn't happen.
+        button.addEventListener('click', config.onclick);
     });
 }
 
+function getInput(){ return document.getElementById(SEARCH_INPUT_ID)};
+
+function searchGame() {
+    const input = getInput();
+    if (!input) { return; }             // Checking to be safe. It shouldn't happen.
+    const value = input.value.trim();
+    if (!value) {  input.setCustomValidity('Inserisci un titolo da cercare');  input.reportValidity(); return;  }
+    
+    input.setCustomValidity(''); 
+    updateFiltersAndRefresh({ Name_c: value }, FILTER_ACTION.ADD);
+}
+
+function clearSearchName() {
+    clearSeachBar();
+    updateFiltersAndRefresh({ Name_c: null }, FILTER_ACTION.REMOVE);
+}
+
 export function clearSeachBar(){
-    const searchInput = document.getElementById(SEARCH_INPUT_ID); 
-    if (searchInput) { searchInput.value = ''; }
+    const input = getInput();
+    if (!input) { return; }             // Checking to be safe. It shouldn't happen.
+    input.value = '';
 }
